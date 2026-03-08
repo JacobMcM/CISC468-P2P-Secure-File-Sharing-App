af8ce2f283efda2c44e9462688245ce6dbf6b783
Cameron Jenkins <camjenkins31@gmail.com>
2026-03-08 19:28:15 -0400

    C++ client: mDNS peer discovery complete (mdns.h, mdns.cpp, main.cpp)

--- C++ Client/src/main.cpp
+++ C++ Client/src/main.cpp
@@ -1,2 +1,24 @@
+#include "mdns.h"
 #include <iostream>
 
+int main() {
+    std::cout << "Starting P2P client..." << std::endl;
+
+    auto registerRef = registerPeer("Cameron Mac", 5001);
+    if (!registerRef) {
+        std::cerr << "Failed to register peer" << std::endl;
+        return 1;
+    }
+
+    auto browseRef = browsePeers();
+    if (!browseRef) {
+        std::cerr << "Failed to start browsing" << std::endl;
+        return 1;
+    }
+
+    runMdnsLoop(registerRef, browseRef);
+
+    DNSServiceRefDeallocate(registerRef);
+    DNSServiceRefDeallocate(browseRef);
+    return 0;
+}
