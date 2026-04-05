10fbbcb1b113f81ef24e1335c9d6996559f6cc8b
Jacob McMullen <jacob.w.mcmullen@gmail.com>
2026-04-05 19:54:41 -0400

    Py Model Updates

--- Python Client/models.py
+++ Python Client/models.py
@@ -2,22 +2,28 @@
 from dataclasses import dataclass, field, asdict
 import json
 import base64
+import util
 
 # --- DH-EKE messages ---
 
-def buildEKE1(sender: str, c1):
-    eke1 = { "type":"EKE_1", "from":sender,"c1":base64.b64encode(c1).decode('utf-8')}
+def buildEKE1(sender: str, c1_bytes: bytes):
+    c1 = util.bytesToB64(c1_bytes)
+    eke1 = { "type":"EKE_1", "from":sender,"c1":c1}
     return json.dumps(eke1)
 
-def buildEKE2(sender: str, c2: str, c3: str):
+def buildEKE2(sender: str, c2_bytes: bytes, c3_bytes: bytes):
+    c2 = util.bytesToB64(c2_bytes)
+    c3 = util.bytesToB64(c3_bytes)
     eke2 = { "type":"EKE_2", "from":sender,"c2":c2,"c3":c3}
     return json.dumps(eke2)
 
-def buildEKE3(sender: str, c4: str):
+def buildEKE3(sender: str, c4_bytes: bytes):
+    c4 = util.bytesToB64(c4_bytes)
     eke3 = { "type":"EKE_3", "from":sender,"c4":c4}
     return json.dumps(eke3)
 
-def buildEKE4(sender: str, c5: str):
+def buildEKE4(sender: str, c5_bytes: bytes):
+    c5 = util.bytesToB64(c5_bytes)
     eke4 = { "type":"EKE_4", "from":sender,"c5":c5}
     return json.dumps(eke4)
 
